e22cb33d539407cdb6b0a4319242b281f7656c72
taighlor <taighlor.mitchell@kingfisher.com>
2016-08-19 15:27:39 +0100

    Update areaRectangle.java

--- areaRectangle.java
+++ areaRectangle.java
@@ -1,10 +1,7 @@
-
 import java.util.Scanner;
-
 public class areaRectangle {
 	private static double length, width, areaFeet, areaMeters;
 	private static final double conversion = 0.09290304;
-	
 	public static void main(String[] args) {
 		Scanner scanUserInput = new Scanner(System.in);		
 			System.out.println("Please insert the rooms length(ft): ");
