bdf894938251e07cef63c39d74d18447d08eb96f
taighlor <taighlor.mitchell@kingfisher.com>
2016-09-14 09:12:46 +0100

    performance enhancements

--- PasswordStrengthIndicator.java
+++ PasswordStrengthIndicator.java
@@ -4,31 +4,35 @@ import java.util.regex.Pattern;
 public class PasswordStrengthIndicator {
 	private static Scanner scanUserInput = new Scanner(System.in);
 	private static String usersPassword;
-	
-	private static Pattern thePasswordLower = Pattern.compile("[a-z]+");
-	private static Pattern thePasswordNumber = Pattern.compile("[0-9]+");
-	private static Pattern thePasswordLowerAndNumber = Pattern.compile("[a-z0-9]+");
-	private static Pattern thePasswordLowerAndSpecial = Pattern.compile("[a-z@!£$%^&*?#~'\"]+");
-	private static Pattern thePasswordAll = Pattern.compile("[a-z0-9@!£$%^&*?#~'\"]+");
-	
+
+	private static String thePasswordLower = "[a-z]+";
+	private static String thePasswordNumber = "[0-9]+";
+	private static String thePasswordLowerAndNumber = "[a-z0-9]+";
+	private static String thePasswordLowerAndSpecial = "[a-z@!£$%^&*?#~'\"]+";
+	private static String thePasswordAll = "[a-z0-9@!£$%^&*?#~'\"]+";
+
 	public void askUserForPassword() {
 		System.out.println("Please insert password: ");
 		usersPassword = scanUserInput.nextLine();
 	}
-	
+
+	private static Pattern getMatcher(String regex) {
+		return Pattern.compile(regex);
+	}
+
 	public static void main(String[] args) {
 		PasswordStrengthIndicator process = new PasswordStrengthIndicator();
 		process.askUserForPassword();
 
-		if (thePasswordLower.matcher(usersPassword).matches()){
+		if (getMatcher(thePasswordLower).matcher(usersPassword).matches()) {
 			System.out.println("Your password is weak ");
-		}	else if (thePasswordNumber.matcher(usersPassword).matches()){
+		} else if (getMatcher(thePasswordNumber).matcher(usersPassword).matches()) {
 			System.out.println("Your password is very weak ");
-		}	else if (thePasswordLowerAndNumber.matcher(usersPassword).matches()){
+		} else if (getMatcher(thePasswordLowerAndNumber).matcher(usersPassword).matches()) {
 			System.out.println("Your password is okay ");
-		}	else if (thePasswordLowerAndSpecial.matcher(usersPassword).matches()){
+		} else if (getMatcher(thePasswordLowerAndSpecial).matcher(usersPassword).matches()) {
 			System.out.println("Your password is safe ");
-		}	else if (thePasswordAll.matcher(usersPassword).matches()){
+		} else if (getMatcher(thePasswordAll).matcher(usersPassword).matches()) {
 			System.out.println("Your password is very safe ");
 		}
 		scanUserInput.close();
